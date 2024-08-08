5fc277f79cacaa67d3906b61543b948c039bbcd1
EuiHyeok <dmlgur0517@daum.net>
2024-08-08 09:12:35 +0900

    [SERVLET]

---
+++ SERVLET/chap07-forward-lecture-source /src/main/java/com/ohgiraffers/section01/forward/PrintLoginSuccessServlet.java
@@ -0,0 +1,41 @@
+package com.ohgiraffers.section01.forward;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/print")
+public class PrintLoginSuccessServlet extends HttpServlet {
+
+    /* 설명. 전달해온(ReceiveInformationServlet) servlet에서 doPost로 전송했음으로 doPost로 받음 */
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("=== 포워딩 된 servlet에서 넘겨받은 request 객체에 담긴 값 확인 ===");
+        /* 설명. 포워딩 시 새로 정의해서 넘겨준 값은 getAttribute로 받음*/
+        System.out.println("이름: " + req.getAttribute("userName"));
+        /* 설명. 포워딩 된 servlet에 포함된 내용은 getParameter로 받음*/
+        System.out.println("아이디: " + req.getParameter("userId"));
+        System.out.println("패스워드: " + req.getParameter("password"));
+
+        String userName = (String)req.getAttribute("userName");
+
+        /* 설명. 출력을 잘하는 Servlet이 jsp와 동일한 개념이다. */
+        StringBuilder responseText = new StringBuilder();
+        responseText.append("<h3 align=\"center\">")
+                    .append(userName)
+                    .append("님 환영합니다.</h3>");
+
+        resp.setContentType("text/html");
+
+        PrintWriter out = resp.getWriter();
+        out.print(responseText);
+        out.flush();
+        out.close();
+    }
+}
