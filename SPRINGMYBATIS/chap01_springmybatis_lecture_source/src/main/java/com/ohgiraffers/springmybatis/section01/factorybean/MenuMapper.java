7381446bf5290ae4f5783f38d4922648b78091cc
EuiHyeok <dmlgur0517@daum.net>
2024-08-22 09:02:45 +0900

    feat: fix

--- SPRINGMYBATIS/chap01_springmybatis_lecture_source/src/main/java/com/ohgiraffers/springmybatis/section01/factorybean/MenuMapper.java
+++
@@ -1,7 +0,0 @@
-package com.ohgiraffers.springmybatis.section01.factorybean;
-
-import java.util.List;
-
-public interface MenuMapper {
-    List<MenuDTO> findAllMenusByOrderableStatus(String orderableStatus);
-}
