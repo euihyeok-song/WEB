f581c60072d4a22ae64259038526be6c8c1dad6b
EuiHyeok <dmlgur0517@daum.net>
2024-09-16 21:11:29 +0900

    [JAVASCRIPT] Array-Distructing-Assignment

--- JAVASCRIPT/02_javacript_es6/03_distructuring-assignment/01_array-distructuring-assignment/01_array-distructuring-assignment.js
+++ JAVASCRIPT/02_javacript_es6/03_distructuring-assignment/01_array-distructuring-assignment/01_array-distructuring-assignment.js
@@ -1,18 +1,19 @@
 
-/* 배열 구조 분해 할당 */
+/* 배열 구조 분해 할당 => 배열 내부의 요소를 내가 원하는 원소 내부에 담는다는 개념 */
 
 let nameArr = ["Gildong", "Hong"];
 // let firstName = nameArr[0];
 // let lastName = nameArr[1];
 
+/* 우측 nameArr가 배열이자, 해당 배열에서 값을 꺼내서, firstName, lastName에 값을 할당 */
 let [firstName, lastName] = nameArr;
 
 console.log('firstName',firstName);
 console.log('lastName',lastName);
 
 /* 문자열에서 구분자를 기준으로 구분하여 각각의 변수에 담는 한줄 코드 */
-let [firstName2, lastName2] = 'Saimdang Shin'.split(' ');
-let [firstName3, lastName3] = 'Saimdang Shin'.match(/[a-z]+/ig);
+let [firstName2, lastName2] = 'Saimdang Shin'.split(' ');  // 배열로 만들어 줌
+let [firstName3, lastName3] = 'Saimdang Shin'.match(/[a-z]+/ig); //대소문자 무시하여 알파벳 찾아 배열로 반환
 
 console.log('firstName2',firstName2);
 console.log('lastName2',lastName2);
@@ -21,4 +22,4 @@ console.log('lastName3',lastName3);
 
 /* 구조분해 할당 시 쉼표를 활용하여 불필요한 배열 요소를 버릴 수도 있다. */
 let [firstName4, , lastName4] = ['firstName', 'middleName', 'lastName'];
-console.log(firstName4, lastName4);
+console.log(firstName4, lastName4);         // 0번 index와 2번 index만 뽑아서 배열에 담음
--- JAVASCRIPT/02_javacript_es6/03_distructuring-assignment/01_array-distructuring-assignment/02_various-usage.js
+++ JAVASCRIPT/02_javacript_es6/03_distructuring-assignment/01_array-distructuring-assignment/02_various-usage.js
@@ -1,5 +1,7 @@
 
 /* 다양한 사용법 */
+
+/* 1. 문자열을 잘라서 배열로 만들고, 배열의 요소를 객체의 property로 넣어줌 */
 let user = {};
 [user.firstName, user.lastName] = "Gwansoon Yu".split(" ");
 
@@ -13,18 +15,19 @@ let [sign1, sign2, ...rest] =
 
 console.log(sign1);
 console.log(sign2);
-console.log(rest);      // 나머지 요소는 배열로 받아짐
+console.log(rest);             // 나머지 요소는 배열로 받아짐
 
 /* 배열 구조 분해 할당을 활용한 변수 교환 */
 let student = '유관순';
 let teacher = '홍길동';
 
-[student, teacher] = [teacher, student];
+/* switching */
+[student, teacher] = [teacher, student];    // 구조분해할당 = 배열
 
 console.log(`학생: ${student}, 교사: ${teacher}`);
 
 /* 기본 값을 설정하고 사용할 수도 있다. */
-let [firstName5 = '아무개', lastName5 = '김'] = ["길동"];
+let [firstName5 = '아무개', lastName5 = '김'] = ["길동"];   // 값을 넣어주지 않은 lastName5에는 default값이 들어감
 
 console.log(firstName5);
 console.log(lastName5);
