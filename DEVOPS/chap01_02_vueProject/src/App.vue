614ff4a09d90b22b13e93acbfcc6152d6f321d40
EuiHyeok <dmlgur0517@daum.net>
2024-10-08 11:03:33 +0900

    [DEVOPS] front 와 back 연결

--- DEVOPS/chap01_02_vueProject/src/App.vue
+++ DEVOPS/chap01_02_vueProject/src/App.vue
@@ -25,8 +25,15 @@
     // cors 해결(frontend)  - http://localhost:5173/api가 vite.config.js에 의해서 http://localhost:7777로 변환됨 (+ /api는 rewrite()를 통해서 삭제 필요)
     // const response =  await fetch(`http://localhost:5173/api/plus?num1=${num1.value}&num2=${num2.value}`);
 
-    // docker의 서버로 띄우기
-    const response = await fetch(`http://localhost:8056/plus?num1=${num1.value}&num2=${num2.value}`);
+    // 8055 docker container(8055:7777로 된 백엔드 서버 컨테이너)로 요청 시(백엔드에서 CORS 처리를 했을 경우)
+    // const response = await fetch(`http://localhost:8055/plus?num1=${num1.value}&num2=${num2.value}`);
+
+    // 프론트에서 CORS 처리를 하고자 하는 경우(프론트 프로젝트가 컨테이너가 아닌 상태)
+    // const response =  await fetch(`http://localhost:5173/api/plus?num1=${num1.value}&num2=${num2.value}`);
+
+    // 프론트에서 CORS 처리를 하고자 하는 경우(프론트 프로젝트가 컨테이너인 상태)
+    const response =  await fetch(`http://localhost:8011/api/plus?num1=${num1.value}&num2=${num2.value}`);  
+
     const data = await response.json();
     result.value = data.sum;
   }
--- DEVOPS/chap01_02_vueProject/vite.config.js
+++ DEVOPS/chap01_02_vueProject/vite.config.js
@@ -18,7 +18,15 @@ export default defineConfig({
     proxy:{
       '/api':{
         // 7777번 포트로 솎여서 7777번 포트로 갈것임을 의미 - 나머지는 속성
-        target: 'http://localhost:7777',
+        // target: 'http://localhost:7777',
+
+        // 프론트에서 CORS 처리를 하고자 하는 경우(프론트 프로젝트가 컨테이너가 아닌 상태)
+        // target: 'http://localhost:8055',
+
+        // docker-compose 및 docker network를 활용해 'springboot-app'이라는 이름으로 통신할 경우(프론트 프로젝트가 컨테이너인 경우)
+        // 백엔드 컨테이너 내부에서 동작하는 포트 번호를 작성해야 한다.(8055:7777)
+        target: 'http://springboot-app:7777',
+
         changeOrigin: true,   
         rewrite: (path) => path.replace(/^\/api/, '')     // /api를 경로에서 지워주는 역할
       }
