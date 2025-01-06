6b854c0560e8dfe6227dfba5fca32e9f51f0a663
EuiHyeok <dmlgur0517@daum.net>
2025-01-06 20:58:31 +0900

    frontend에서 CORS 설정

--- DEVOPS/chap01_02_vueProject/src/App.vue
+++ DEVOPS/chap01_02_vueProject/src/App.vue
@@ -1,3 +1,4 @@
+<!-- 처음 vue project를 만들때, npm create vue@latest로 만드는데 이를 sfc(single file component)를 사용해서 component를 file단위로 하나씩 만든다. -->
 <template>
   <div class="plus">
     <h1>덧셈 기능 만들기</h1>
@@ -12,6 +13,7 @@
 <script setup>
   import { ref } from 'vue';
 
+  // ref로 반응형 변수로 관리 => script에서 변수가 바뀌면 자동으로 template에도 적용됨
   const num1 = ref(0);
   const num2 = ref(0);
   const result = ref(0);
@@ -20,10 +22,12 @@
   const sendPlus = async() => {
 
     // cors 해결(backend)
-    // const response = await fetch(`http://localhost:7777/plus?num1=${num1.value}&num2=${num2.value}`);     // response에는 num1, num2, sum이 넘어옴
+    // response에는 num1, num2, sum이 넘어옴
+    // const response = await fetch(`http://localhost:7777/plus?num1=${num1.value}&num2=${num2.value}`); 
 
-    // cors 해결(frontend)  - http://localhost:5173/api가 vite.config.js에 의해서 http://localhost:7777로 변환됨 (+ /api는 rewrite()를 통해서 삭제 필요)
-    // const response =  await fetch(`http://localhost:5173/api/plus?num1=${num1.value}&num2=${num2.value}`);
+    // cors 해결(frontend) 
+    // http://localhost:5173/api가 vite.config.js에 의해서 http://localhost:7777로 변환됨 (+ /api는 rewrite()를 통해서 삭제 필요)
+    // const response =  await fetch(`http://localhost:5173/api/plus?num1=${num1.value}&num2=${num2.value}`); 
 
     // 8055 docker container(8055:7777로 된 백엔드 서버 컨테이너)로 요청 시(백엔드에서 CORS 처리를 했을 경우)
     // const response = await fetch(`http://localhost:8055/plus?num1=${num1.value}&num2=${num2.value}`);
