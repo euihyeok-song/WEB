dbca1b386e1ddd9e955c87a5a9fc58b9773a5c93
EuiHyeok <dmlgur0517@daum.net>
2025-01-15 21:17:47 +0900

    백엔드 파일 Docker  띄워 프론트랑 연결

--- DEVOPS/chap01_01_bootProject/src/main/java/com/ohgiraffers/bootproject/config/WebConfig.java
+++ DEVOPS/chap01_01_bootProject/src/main/java/com/ohgiraffers/bootproject/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-//                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173")
 
                 // k8s 적용
                 // Ingress 적용 이전 프론트 워커노드 포트에 대한 CORS 처리
@@ -20,7 +20,7 @@ public class WebConfig implements WebMvcConfigurer {
 
                 // Ingress 적용 이후 CORS 불필요로 인한 경로 제거
                 // 외부에서 들어와서 CORS 처리가 필요할 수도 있으니 @EnableWebMvc를 주석처리 해도 좋으나 아래와 같은 방식 채택
-                .allowedOrigins()
+                // .allowedOrigins()
                 .allowedMethods("GET", "POST", "PUT" , "DELETE");
     }
 }
