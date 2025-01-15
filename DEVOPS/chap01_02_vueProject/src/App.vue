065ac075813e8365dfe4870989527457cdf59dea
EuiHyeok <dmlgur0517@daum.net>
2025-01-15 21:23:25 +0900

    백엔드 docker 이미지  맞게 경로 지정

--- DEVOPS/chap01_02_vueProject/src/App.vue
+++ DEVOPS/chap01_02_vueProject/src/App.vue
@@ -30,7 +30,7 @@
     // const response =  await fetch(`http://localhost:5173/api/plus?num1=${num1.value}&num2=${num2.value}`); 
 
     // 8055 docker container(8055:7777로 된 백엔드 서버 컨테이너)로 요청 시(백엔드에서 CORS 처리를 했을 경우)
-    // const response = await fetch(`http://localhost:8055/plus?num1=${num1.value}&num2=${num2.value}`);
+    const response = await fetch(`http://localhost:8056/plus?num1=${num1.value}&num2=${num2.value}`);
 
     // 프론트에서 CORS 처리를 하고자 하는 경우(프론트 프로젝트가 컨테이너가 아닌 상태)
     // const response =  await fetch(`http://localhost:5173/api/plus?num1=${num1.value}&num2=${num2.value}`);
@@ -42,7 +42,7 @@
     // const response =  await fetch(`http://localhost:30001/plus?num1=${num1.value}&num2=${num2.value}`);  
 
     // Ingress를 활용한 절대 경로로 통신
-    const response = await fetch(`/boot/plus?num1=${num1.value}&num2=${num2.value}`);
+    // const response = await fetch(`/boot/plus?num1=${num1.value}&num2=${num2.value}`);
 
     const data = await response.json();
     result.value = data.sum;
