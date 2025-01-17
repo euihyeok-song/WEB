e2e5ab2968c71579db35af684904cdcf31c6ba79
EuiHyeok <dmlgur0517@daum.net>
2025-01-17 17:16:37 +0900

    백엔드 & 프론트엔드 컨테이너에 띄워서 CORS 처리

--- DEVOPS/chap01_02_vueProject/src/App.vue
+++ DEVOPS/chap01_02_vueProject/src/App.vue
@@ -29,17 +29,17 @@
     // http://localhost:5173/api가 vite.config.js에 의해서 http://localhost:7777로 변환됨 (+ /api는 rewrite()를 통해서 삭제 필요)
     // const response =  await fetch(`http://localhost:5173/api/plus?num1=${num1.value}&num2=${num2.value}`); 
 
-    // 8055 docker container(8055:7777로 된 백엔드 서버 컨테이너)로 요청 시(백엔드에서 CORS 처리를 했을 경우)
+    // 8056 docker container(8056:7777로 된 백엔드 서버 컨테이너)로 요청 시(백엔드에서 CORS 처리를 했을 경우)
     // const response = await fetch(`http://localhost:8056/plus?num1=${num1.value}&num2=${num2.value}`);
 
     // 8055 docker container(8055:7777로 된 백엔드 서버 컨테이너)로 요청 시(프론트에서 CORS 처리를 했을 경우) - cors 처리 문제 발생 
     // const response = await fetch(`http://localhost:8055/plus?num1=${num1.value}&num2=${num2.value}`);
 
     // 프론트에서 CORS 처리를 하고자 하는 경우(프론트 프로젝트가 컨테이너가 아닌 상태)
-    const response =  await fetch(`http://localhost:5173/api/plus?num1=${num1.value}&num2=${num2.value}`);
+    // const response =  await fetch(`http://localhost:5173/api/plus?num1=${num1.value}&num2=${num2.value}`);
 
     // 프론트에서 CORS 처리를 하고자 하는 경우(프론트 프로젝트가 컨테이너인 상태)
-    // const response =  await fetch(`http://localhost:8011/api/plus?num1=${num1.value}&num2=${num2.value}`); 
+    const response =  await fetch(`http://localhost:8011/api/plus?num1=${num1.value}&num2=${num2.value}`); 
     
     // k8s를 워커노드들로 띄워지고 난 이후(백엔드: 30001, 프론트엔드:30000) - Ingress 적용 이전 30001번의 백엔드 워커노드와 통신
     // const response =  await fetch(`http://localhost:30001/plus?num1=${num1.value}&num2=${num2.value}`);  
--- DEVOPS/chap01_02_vueProject/vite.config.js
+++ DEVOPS/chap01_02_vueProject/vite.config.js
@@ -21,11 +21,11 @@ export default defineConfig({
         // target: 'http://localhost:7777',
 
         // 프론트에서 CORS 처리를 하고자 하는 경우(프론트 프로젝트가 컨테이너가 아닌 상태)
-        target: 'http://localhost:8055',
+        // target: 'http://localhost:8055',
 
         // docker-compose 및 docker network를 활용해 'springboot-app'이라는 이름으로 통신할 경우(프론트 프로젝트가 컨테이너인 경우)
-        // 백엔드 컨테이너 내부에서 동작하는 포트 번호를 작성해야 한다.(8055:7777)
-        // target: 'http://springboot-app:7777',
+        // 백엔드 컨테이너 내부에서 동작하는 포트 번호를 작성해야 한다.(8055:7777) => CNI를 통해서 vue와 springboot가 바로 통신함으로 7777로 보냄
+        target: 'http://springboot-app:7777',
 
         changeOrigin: true,   
         rewrite: (path) => path.replace(/^\/api/, '')     // /api를 경로에서 지워주는 역할
